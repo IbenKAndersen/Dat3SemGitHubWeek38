0dcfe4ca8badb639007982e647883734003b5bd6
Iben A <cph-ia62@cphbusiness.dk>
2019-09-20 14:38:29 +0200

    Create StudentInfo.java

---
+++ 03_friday_excercises/ExamPrep_JPQL/src/main/java/mappers/StudentInfo.java
@@ -0,0 +1,14 @@
+package mappers;
+
+/**
+ *
+ * @author ibenk
+ */
+public class StudentInfo {
+
+    public String fullName;
+    public long studentId;
+    public String classNameThisSemester;
+    public String classDescription;
+
+}
